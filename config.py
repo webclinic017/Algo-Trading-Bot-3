fc8aff58326b68876cec472c5c1298ebf6554ef5
pranavgudise <130630160+pranavgudise@users.noreply.github.com>
2023-04-13 00:25:55 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = "soVOZctpUXlzPRC77NmdYCo3ac3gM0xtXFPfwpzy"
+API_KEY = "AK2EG11TF1UW3G3GJ9KL"
+
+ALPACA_CONFIG = {
+    "API_KEY" : API_KEY,
+    "API_SECRET" : SECRET_KEY,
+    "ENDPOINT" : "https://paper-api.alpaca.markets"
+}
